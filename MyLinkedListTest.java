45dbfccad277dc31a71be9f924843bd468067adf
CoderArsh <arshjeetb08@gmail.com>
2021-10-19 19:31:51 +0530

    Added: to pop last element of linked list

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -69,6 +69,23 @@ public class MyLinkedListTest {
 
     }
 
+    @Test
+    public void given3NumbersRemovingLastElementFromTheList() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.popLast(myThirdNode);
+
+        boolean result = myLinkedList.head.equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+
+    }
+
 
 }
 
