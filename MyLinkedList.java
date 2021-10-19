0e8cdee3a15bf02f9827e48457ca42b79bc5ca51
CoderArsh <arshjeetb08@gmail.com>
2021-10-19 17:21:09 +0530

    Added: Insert method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -37,6 +37,12 @@ public class MyLinkedList {
 
     }
 
+    public void insert(INode myNode, INode newNode) {
+        INode tempNode = myNode.getNext();
+        myNode.setNext(newNode);
+        newNode.setNext(tempNode);
+    }
+
 
 
 
--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -36,6 +36,22 @@ public class MyLinkedListTest {
 
     }
 
+    @Test
+    public void given3NumbersWhenInsertingSecoundInBetweenShouldPassLinkedListResult() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode, mySecondNode);
+        boolean result = myLinkedList.head.equals(myFirstNode)&&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+
+    }
+
 
 
 
